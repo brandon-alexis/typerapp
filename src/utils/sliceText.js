9f47fb4fad2920a1f5132e5ff656f62a5c81cccf
brandon-alexis <brandonmachado159@gmail.com>
2024-04-24 09:47:17 -0500

    agregar metodo para separacion de cadenas

---
+++ src/utils/sliceText.js
@@ -0,0 +1,11 @@
+const sliceText = (text) => {
+  let arrayText = []
+
+  for (let i = 0; i < text.length; i++) {
+    arrayText.push(text[i])
+  }
+
+  return arrayText
+}
+
+export default sliceText
